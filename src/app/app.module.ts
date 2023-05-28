51dabd01913da1a67c1de4cabb302fda13962ece
DevMansuri <nadimmansu@gmail.com>
2023-05-28 11:40:26 +0530

    Updates files : update in login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,8 @@ import { LoginPageComponent } from './components/pages/login-page/login-page.com
 import { ReactiveFormsModule } from '@angular/forms';
 import { ToastrModule } from 'ngx-toastr';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { InputContainerComponent } from './components/partials/input-container/input-container.component';
+import { InputValidatonComponent } from './components/partials/input-validaton/input-validaton.component';
 
 @NgModule({
   declarations: [
@@ -28,7 +30,9 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
     CartPageComponent,
     TitleComponent,
     NotFoundComponent,
-    LoginPageComponent
+    LoginPageComponent,
+    InputContainerComponent,
+    InputValidatonComponent
   ],
   imports: [
     BrowserModule,
